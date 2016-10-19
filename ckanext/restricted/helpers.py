073c08e3d5bb03922ed9b4c62b175bc554dca9dc
Lucia Espona <lucia.espona@wsl.ch>
2016-10-19 12:42:35 +0200

    getting user orgs from logic

--- ckanext/restricted/helpers.py
+++ ckanext/restricted/helpers.py
@@ -1,10 +1,11 @@
 import logging
+
+import ckan.logic as logic
 from ckan.common import c
 
 logger = logging.getLogger(__name__)
 
 def restricted_check_user_resources_access(user, restricted = 'registered'):
-    import ckan.lib.helpers as h
 
     # Public resources
     if restricted == 'public':
@@ -18,11 +19,14 @@ def restricted_check_user_resources_access(user, restricted = 'registered'):
 
     # Get organization list
     user_organization_list = []
-    for org in h.organizations_available(permission='read'):
+
+    context = {'user': user}
+    data_dict = {'permission': 'read'}
+
+    for org in logic.get_action('organization_list_for_user')(context, data_dict):
         name = org.get('name', '')
         if name:
             user_organization_list += [name]
-    logger.debug ('restricted_check_user_resources_access: organizations for ' + str(user) + ': ' + ",  ".join(user_organization_list))
 
     # Any Organization Members (Trusted Users)
     if not user_organization_list:
@@ -30,7 +34,6 @@ def restricted_check_user_resources_access(user, restricted = 'registered'):
     if restricted == 'any_organization':
         return True
     pkg_organization_name = c.pkg_dict.get('organization', {'name':' '}).get('name', ' ')
-    logger.debug('restricted_check_user_resources_access: package organization ' + pkg_organization_name)
 
     # Same Organization Members
     if restricted == 'same_organization':
