f576de57b83f5460bc99fb7206618d2fcdb7f49b
Lucia Espona Pernas <espona@users.noreply.github.com>
2016-10-25 11:06:24 +0200

    Update travis-build.bash

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -7,16 +7,36 @@ echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
 sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
 
+echo "Patching lxml..."
+wget ftp://xmlsoft.org/libxml2/libxml2-2.9.0.tar.gz
+tar zxf libxml2-2.9.0.tar.gz
+cd libxml2-2.9.0/
+./configure --quiet --libdir=/usr/lib/x86_64-linux-gnu
+make --silent
+sudo make --silent install
+xmllint --version
+cd -
+
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
-echo "CKAN branch: $latest_ckan_release_branch"
-git checkout $latest_ckan_release_branch
+if [ $CKANVERSION != 'master' ]
+then
+    git checkout release-v$CKANVERSION-latest
+fi
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
 cd -
+
+echo "Setting up Solr..."
+# solr is multicore for tests on ckan master now, but it's easier to run tests
+# on Travis single-core still.
+# see https://github.com/ckan/ckan/issues/2972
+sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
+printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
 
 echo "Creating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
@@ -26,6 +46,9 @@ echo "Initialising the database..."
 cd ckan
 paster db init -c test-core.ini
 cd -
+
+echo "Fixing issue with pytz..."
+pip install pytz==2016.4
 
 echo "Installing ckanext-restricted and its requirements..."
 python setup.py develop
