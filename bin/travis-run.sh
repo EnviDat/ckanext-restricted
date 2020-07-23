5791b1e25283d4c9c6dc4b3ecaaa2a7e07c45ece
Tomasz Sabała <sabala.tomek@gmail.com>
2020-07-23 12:48:30 +0200

    Removing the error argument.

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -3,7 +3,7 @@ set -ex
 
 flake8 --version
 # stop the build if there are Python syntax errors or undefined names
-flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan,{{ project }}
+flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan
 
 nosetests --ckan \
           --nologcapture \
@@ -16,4 +16,4 @@ nosetests --ckan \
           ckanext/restricted
 
 # strict linting
-flake8 . --count --max-complexity=10 --max-line-length=127 --statistics --exclude ckan,{{ project }}
+flake8 . --count --max-complexity=10 --max-line-length=127 --statistics --exclude ckan
